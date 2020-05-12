ae1ea4418be37c15557e40629ec143f36f79e8c4
fredegrec <fredegrec@gmail.com>
2020-05-12 08:39:19 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ LOGS_FILE = 'trellobot.logs'
 
 #trello
 APP_KEY = 'blablabla' #trello's admin api key https://trello.com/app-key
-ORIGIN = 'http://209.97.150.98:5000' # server address where the bot code is running(domain or http://ip:port), you need to insert it into allowed origins of the  trello's admin's account on the page https://trello.com/app-key
+ORIGIN = 'http://209.97.150.98:5000' # server address where the bot code is running(for example, http://ip:port), you need to insert it into allowed origins of the  trello's admin's account on the page https://trello.com/app-key
 HOST = '209.97.150.98' #server IP where the bot code is running
 PORT = 5000 # port, where bot's server listen
 URL_TEMPLATE = 'https://trello.com/1/authorize?expiration=never&name=Dialog&scope=read,write,account&key={}&return_url={}/users/{}&callback_method=fragment'
